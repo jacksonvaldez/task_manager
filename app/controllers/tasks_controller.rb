923a5705f3fa1842e5f8d9fa8d46d52d4f370994
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-27 16:35:49 -0700

    Complete task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
@@ -9,13 +9,38 @@ class TasksController < ApplicationController
   end
 
   def create
+    # binding.pry
     task = Task.new({
-      title: params[:title],
+      title: params[:task][:title],
       description: params[:task][:description]
       })
-
+    # binding.pry
     task.save
+    # binding.pry
+    redirect_to '/tasks'
+  end
 
+  def show
+    # binding.pry
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
     redirect_to '/tasks'
   end
 
